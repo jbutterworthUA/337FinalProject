865601ef87bd8db75db78476b4766423e7c957d6
Jason Butterworth <jasonbutter04@gmail.com>
2025-12-02 14:54:29 -0700

    server.js commenting.

--- server.js
+++ server.js
@@ -12,3 +12,29 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "start.html"))
 });
 
+// For when user has just submitted their new account details.
+app.post("/signup", (req, res) => {
+    // Get the username and password for the account user just created.
+    const myUsername = req.body.username;
+    const myPassword = req.body.password;
+
+    // ***TODO: Add the new user info to users.json.
+
+    // Now that info has been saved, send the user to the main catalog page.
+    res.sendFile(path.join(__dirname, "catalog.html"));
+});
+
+// For when the user has just tried to login.
+app.post("/login", (req, res) => {
+    // Get the username and password the user has entered.
+    const myUsername = req.body.username;
+    const myPassword = req.body.password;
+
+    // ***TODO:
+    // Verify that the username and password match one of the entries in users.json.
+
+    // For every obj in users.json, if user.username === myUsername, check if myPassword === user.password.
+
+    // If so, app.sendFile() to catalog.html like with /login.
+});
+
