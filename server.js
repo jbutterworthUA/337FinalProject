ba186422b75f3b476c95a1e7cfc576929abe3abe
Jason Butterworth <jasonbutter04@gmail.com>
2025-12-02 20:53:29 -0700

    Connecting server.js-catalog.html

--- server.js
+++ server.js
@@ -8,10 +8,16 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 
+// Needing these so we can do req.query, etc post stuff with express.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Need this to be able to serve the database files since they're in a different folder.
+app.use(express.static(__dirname));
+
 // Helper function to get the full list of users.
 function getUserList() {
     // Use FS to read the file and then add each user item to the list.
-    const myUsers = fs.readFileSync("databases/users.json", "utf-8");
+    const myUsers = fs.readFileSync(path.join(__dirname, "databases/users.json"), "utf-8");
     
      // If it was empty bc there's no users yet make sure it just returns empty list.
      if (myUsers === "") {
@@ -34,7 +40,7 @@ function updateUserJSON(userList) {
 
 // When user first runs in their browser, send them to start.
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "start.html"));
+    return res.sendFile(path.join(__dirname, "start.html"));
 });
 
 // For when user has just submitted their new account details.
@@ -48,10 +54,10 @@ app.post("/signup", (req, res) => {
 
     // ***TODO: If the username already exists, then they need to do it again.
     // Rn this will just send back to the signup page without saying why.
-    for (user of userList) {
+    for (let user of userList) {
         const currName = user.username;
         if (currName === newUsername) {
-            res.sendFile(path.join(__dirname, "signup.html"));
+            return res.sendFile(path.join(__dirname, "signup.html"));
         }
     }
 
@@ -67,8 +73,17 @@ app.post("/signup", (req, res) => {
     // Update the JSON file.
     updateUserJSON(userList);
 
+    // IMPORTANT: Now that the user has signed up, they will get their own .json file
+    // so we can keep track of how which movies they have flagged for their watchlist.
+    // Will be a copy of the "master" catalog.json, and we will only flag the movie in their personal json file when they bookmark one.
+    const masterCatalog = fs.readFileSync("databases/catalog.json", "utf-8");
+
+
+    fs.writeFileSync(`databases/user_catalogs/${newUsername}_catalog.json`, masterCatalog);
+
     // Now that info has been saved, send the user to the main catalog page.
-    res.sendFile(path.join(__dirname, "catalog.html"));
+    // TODO: Possibly change how to keep track of logged in user instead of having res.redirect. *****
+    return res.redirect(`/catalog?username=${newUsername}`);
 });
 
 // For when the user has just tried to login.
@@ -82,19 +97,22 @@ app.post("/login", (req, res) => {
 
     // Verify that the username and password match one of the entries in users.json.
     // Loop over all existing users.
-    for (user of userList) {
+    for (let user of userList) {
         const currName = user.username;
         const currPwd = user.password;
         // If username and password match an entry, send user to the main catalog page.
         if (currName === myUsername && currPwd === myPassword) {
-            res.sendFile(path.join(__dirname, "catalog.html"));
+            // TODO: Possibly change how to keep track of logged in user instead of having res.redirect. *****
+            return res.redirect(`/catalog?username=${myUsername}`);
         }
     }
     // At this point the user entered invalid credentials. Need to go back to the login page.
     // TODO: Also need to tell them they entered something invalid. ***
-    res.sendFile(path.join(__dirname, "login.html"));
+    return res.sendFile(path.join(__dirname, "login.html"));
 });
 
-// That should be all of the app.posts(). Everything else should be hrefs to jump btwn pages, plus logout button to send back to start.html.
+app.get("/catalog", (req, res) => {
+    return res.sendFile(path.join(__dirname, "catalog.html"));
+});
 
 app.listen(3000, () => {console.log("Server running on http://localhost:3000")});
